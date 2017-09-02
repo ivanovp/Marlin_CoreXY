675a8f868b8f61dbc92c947d4146634d9d741377
Peter Ivanov <ivanovp@gmail.com>
2017-09-02 12:23:56 +0200

    Max. acceleration of Z was decreased.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -559,7 +559,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
 //#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 8, 10000 }
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 8, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
